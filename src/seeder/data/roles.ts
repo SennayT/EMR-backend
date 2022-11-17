9f2db6007cdf39bee3734e5a79bae435c8a067c0
Sennay <wsennay@gmail.com>
2022-11-17 14:53:33 +0300

    add role data

---
+++ src/seeder/data/roles.ts
@@ -0,0 +1,15 @@
+enum Roles {
+  Radiologist = 'Radiologist',
+  Patient = 'Patient',
+  Doctor = 'Doctor',
+  Receptionist = 'Receptionist',
+  Employee = 'Employee',
+  Researcher = 'Researcher',
+  Nurse = 'Nurse',
+  LabExpert = 'LabExpert',
+  System_Admin = 'System Admin',
+  Hospital_Admin = 'Hospital Admin',
+  Moh_Employee = 'Moh Employee',
+}
+
+export { Roles };
